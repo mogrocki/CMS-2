cd818181e1c22bab1467e9d404bdca7a1b76a056
Michal Ogrockigit config --global user.name Michal <m.ogrocki@gmail.com>
2015-04-27 21:07:05 +0200

    CMS-2

---
+++ application/config/generated_overrides/concrete.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------
+ * Generated 2015-04-27T19:56:06+02:00
+ *
+ * @item      misc.access_entity_updated
+ * @group     concrete
+ * @namespace null
+ * -----------------------------------------------------------------------------
+ */
+return array(
+    'site' => 'victoria',
+    'version_installed' => '5.7.3.1',
+    'misc' => array(
+        'access_entity_updated' => 1430157366,
+        'latest_version' => '5.7.3.1',
+        'seen_introduction' => true
+    )
+);
---
+++ application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/ecc85fb614566729/ce47dff11c3c2d8f.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_3781cc8b734cd1053a2b81262e8b9a72_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1430575210;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1430156787;
